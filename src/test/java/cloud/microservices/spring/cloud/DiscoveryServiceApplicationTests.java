2567c3399f461d0b3fb71f4a31aca9d0becc4985
Ratish Vichithran <ratish.vichithran@gmail.com>
2019-12-31 08:44:35 -0500

    Initial Commit

---
+++ src/test/java/cloud/microservices/spring/cloud/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.microservices.spring.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
